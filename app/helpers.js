03b8266d56bab614011673668835197f8ff0406c
jkacens <jkacens@dyninno.lv>
2017-04-25 21:36:02 +0300

    organizing files, fix bugs

--- app/helpers.js
+++ app/helpers.js
@@ -89,55 +89,6 @@ const hypheniphyDate = (date) => {
 }
 
 
-const nameDayAction = (event, action) => {
-    const param = event.message && event.message.replace(/^!vd\s?/, '');
-
-    const datePattern = /(\d{1,2})[/-](\d{1,2})/
-
-    if (!param) {
-        let date = getDate()
-        const names = vdLib[date.short]
-        const extendedNames = vdExd[date.short]
-
-        event[action](
-            `Vārda dienu šodien, ${date.full}, `
-            + `svin ${BOLD_CHAR + names.join(', ') + BOLD_CHAR}, `
-            + `kā arī ${extendedNames.join(', ')}.`
-        )
-    } else if (datePattern.test(param)) {
-        let [input, month, day] = param.match(datePattern)
-        month = _.padStart(month, 2, '0')
-        day = _.padStart(day, 2, '0')
-
-        const key = `${month}-${day}`
-
-        const names = vdLib[key]
-        const extendedNames = vdExd[key]
-        if (!names || !extendedNames) {
-            return
-        }
-
-        event[action](
-            `Vārda dienu ${getFullDateName(month, day)} `
-            + `svin ${BOLD_CHAR + names.join(', ') + BOLD_CHAR}, `
-            + `kā arī ${extendedNames.join(', ')}.`
-        )
-    } else {
-        const key = _.findKey(vdLib, s => s.indexOf(param) !== -1)
-        const extendedKey = key ? null : _.findKey(vdExd, s => s.indexOf(param) !== -1)
-
-        if (!key && !extendedKey) {
-            event[action](`${param} nesvin.`)
-            return
-        }
-
-        let [month, day] = (key || extendedNames).split('-')
-        month = +month
-        day = +day
-
-        event[action](`${param} vārda dienu svin ${getFullDateName(month, day)}.`);
-    }
-}
 
 module.exports = {
     humanizeDelta,
@@ -147,6 +98,5 @@ module.exports = {
     removeFromMemory,
     checkIfExists,
     hypheniphyDate,
-    nameDayAction,
     DURATION_MAPPING
 }
