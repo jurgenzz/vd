24b67a0168ee8c0c4a2f3e62f3e891d5af3fc0ef
Jurgis Kacens <jurgis.kacens@outlook.com>
2018-01-13 21:32:01 +0200

    naming stuff

--- app/Commands.js
+++ app/Commands.js
@@ -1,4 +1,5 @@
 const { cmdList } = require("./cmdList");
+let Storage = require("node-storage");
 
 class CommandsClass {
   /**
@@ -11,13 +12,34 @@ class CommandsClass {
   handleCommand(command, message, event, vdk) {
     if (command && cmdList[command]) {
       cmdList[command](message, event, vdk);
+    } else {
+      this.handleCommandFromStorage(command, event)
     }
   }
 
   /**
    *
+   * @param {string} command
+   * @param {string} event
    */
-  handleCommandFromStorage() {}
+  handleCommandFromStorage(command, event) {
+    let db = new Storage("../db");
+    let UIcommands = db.get("commands");
+    if (UIcommands) {
+      UIcommands = JSON.parse(UIcommands);
+    }
+    let uiMessage = event.message.split(" ").slice(1);
+    uiMessage = uiMessage.join(" ");
+    const cmd = event.message.split(" ")[0];
+  
+    if (UIcommands[cmd]) {
+      const reply = UIcommands[cmd]
+        .replace(/{urlParam}/, encodeURIComponent(uiMessage))
+        .replace(/{param}/, uiMessage)
+        .replace(/{nick}/, event.nick);
+      event.reply(reply);
+    }
+  }
 }
 
 let Commands = new CommandsClass();
